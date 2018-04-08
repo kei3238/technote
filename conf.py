49a6e54e7952da0fd7a987ebd97717cda706d4bb
Yusuke Kikuchi <yusuke.k11@gmail.com>
2018-04-08 15:47:14 +0900

    add docs directory to exclude_patterns

--- conf.py
+++ conf.py
@@ -71,7 +71,7 @@ language = 'jp'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'docs']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
